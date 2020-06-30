61141b79e7b0a879d4fa7eb41f7a075b6ff60114
Kuntal Majumder <hellozee@disroot.org>
2020-07-01 02:24:55 +0530

    Shifted right to left

--- CoolBox.py
+++ CoolBox.py
@@ -293,7 +293,7 @@ class CoolBox(DockWidget):
 
 instance = Krita.instance()
 dock_widget_factory = DockWidgetFactory(DOCKER_ID, 
-                                        DockWidgetFactoryBase.DockRight, 
+                                        DockWidgetFactoryBase.DockLeft, 
                                         CoolBox)
 
 instance.addDockWidgetFactory(dock_widget_factory)
